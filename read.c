fa1455d2869d6f16db0f10c50620bf86c2443886
Aaron Brady <aaron.brady@shopify.com>
2023-11-16 21:16:28 -0500

    Mostly switched to double and float

--- read.c
+++ read.c
@@ -8,14 +8,15 @@
 #include <pulse/simple.h>
 
 #define RATE 48000
-#define BUFZ 10000
+#define BUFZ 7200
 #define CR 10
 
 int main(int c, char **v) {
   pa_simple *s;
   pa_sample_spec ss;
 
-  ss.format = PA_SAMPLE_S16NE;
+  // ss.format = PA_SAMPLE_S16NE;
+  ss.format = PA_SAMPLE_FLOAT32NE;
   ss.channels = 1;
   ss.rate = RATE;
 
@@ -29,10 +30,10 @@ int main(int c, char **v) {
     return -1;
   }
 
-  int16_t buf[BUFZ] = {0};
+  float buf[BUFZ] = {0};
 
-  int crossings = 0;
-  int since_last_crossing = 0;
+  double crossings = 0;
+  double since_last_crossing = 0;
   double average_crossing = 0;
   double hz_guess = 0;
   bool was_negative = false;
@@ -47,10 +48,11 @@ int main(int c, char **v) {
     for (int y = 0; y < BUFZ; y++) {
       int16_t s = buf[y];
       if ((s < 0 && !was_negative) || (s >= 0 && was_negative)) {
+        since_last_crossing++;
         crossings++;
         if (crossings >= CR) {
-          average_crossing = since_last_crossing;
-          hz_guess = 0.5 / (average_crossing / (RATE * crossings));
+          average_crossing = since_last_crossing / crossings;
+          hz_guess = 0.5 / (average_crossing / RATE);
           since_last_crossing = 0;
           crossings = 0;
         }
@@ -59,9 +61,8 @@ int main(int c, char **v) {
         since_last_crossing++;
       }
     }
-    printf("MA of interval: %f, guess is %.02fHz\n", average_crossing,
+    printf("\rMA of interval: %f, guess is %.02fHz            ", average_crossing,
            hz_guess);
-    printf("There were %d crossings.\n", crossings);
     fflush(stdout);
   }
 }
